8ded4452c920ef0fb31da33eb9276c2d0ca96324
vmaevski <vmaevski@mail.ru>
2022-07-01 20:46:45 +0700

    Created a procedure for creating an array of strings.

---
+++ Task/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter number of rows:");
+int length = int.Parse(Console.ReadLine());
+string[] inputArray = new string[length];
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine("Enter the string:");
+    inputArray[i] = Console.ReadLine();
+}
+
+for (int i = 0; i < length; i++)
+{   
+    Console.WriteLine(inputArray[i]);
+}
