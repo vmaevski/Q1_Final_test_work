6a2f91439ca8861e3aa01fdc837701bd8b4277c4
vmaevski <vmaevski@mail.ru>
2022-07-01 21:57:39 +0700

    Created a procedure for generating the output array.

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,4 +1,26 @@
-﻿void PrintArray(string[] array)
+﻿string[] CreateOutputArray(string[] inArr)
+{
+    int inLength = inArr.Length;
+    int outLength = 0;
+    int count = 0;
+    for (int i = 0; i < inLength; i++)
+    {
+        if (inArr[i].Length <= 3) outLength++;
+    }
+    string[] outputArray = new string[outLength];
+    for (int i = 0; i < inLength; i++)
+    {
+        if (inArr[i].Length <= 3)
+        {
+            outputArray[count] = inArr[i];
+            count++;
+        }
+
+    }
+    return outputArray;
+}
+
+void PrintArray(string[] array)
 {
     Console.Write("[");
     int length = array.Length;
@@ -7,12 +29,13 @@
         if (i < length - 1)
         {
             Console.Write($"{array[i]}, ");
-        }    
-        else 
+        }
+        else
         {
-            Console.Write($"{array[i]}]");
+            Console.Write($"{array[i]}");
         }
     }
+    Console.Write("]");
 }
 
 Console.WriteLine("Enter number of rows:");
@@ -20,10 +43,10 @@ int length = int.Parse(Console.ReadLine());
 string[] inputArray = new string[length];
 for (int i = 0; i < length; i++)
 {
-    Console.WriteLine("Enter the string:");
+    Console.WriteLine($"Enter the {i + 1}th string:");
     inputArray[i] = Console.ReadLine();
 }
 
 PrintArray(inputArray);
 Console.Write(" -> ");
-PrintArray(inputArray);
+PrintArray(CreateOutputArray(inputArray));
